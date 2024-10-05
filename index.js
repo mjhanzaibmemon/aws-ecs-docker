16bb3b360cb24157c372a76e6471f06e9583007e
MUHAMMAD JAHANZEB <53127947+mjhanzaibmemon@users.noreply.github.com>
2024-10-05 22:13:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
+// Public folder ko static serve karne ke liye set karein
+app.use(express.static(path.join(__dirname, 'public')));
+
+// "/" route ke liye index.html serve karo
 app.get("/", (req, res) => {
-  res.send("Welcome to my app hiiiiiii");
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// /info route ke liye ek simple message return karein
 app.get("/info", (req, res) => {
-  res.send("hey this is /info api");
+  res.send("Hey, this is the /info API");
 });
 
+// Server ko port 3000 pe sunao
 app.listen(3000, () => {
-  console.log("listening on 3000");
+  console.log("Server is listening on port 3000");
 });
